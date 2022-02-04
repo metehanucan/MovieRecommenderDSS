27f23f6f811e5011f34664b6bd5eb3ecde35d1d9
metehanucan <76853222+metehanucan@users.noreply.github.com>
2022-02-05 00:10:48 +0300

    Update script.py

--- script.py
+++ script.py
@@ -31,3 +31,39 @@ def read_mongo(db, collection, query={}, no_id=True):
     return df
 
 df = read_mongo(db='test',collection='movies',query={})
+df['description'] = df['overview'] + df['title'] + df['genres'].fillna('').str.join(' ')  + 
+df['cast'].fillna('').str.join(' ') + 
+df['crew'].apply(lambda d: d if isinstance(d, list) else []).apply(lambda x: list(filter(lambda a: a['job'] == 'Director', x))).apply(lambda d: list(map(lambda c: c['name'],d))).str.join(' ')
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+import re
+stemmer = PorterStemmer()
+def stem_words(words_list, stemmer):
+  return [stemmer.stem(word) for word in words_list]
+
+def tokenizer(text):
+  letters_only = re.sub("[^a-zA-Z]",' ', text)
+  words = letters_only.lower().split()
+  stopwords_eng = set(stopwords.words("english"))
+  useless_words = ['film','movie','available','theater','directed']
+  stopwords_eng.update(useless_words)
+  useful_words = [x for x in words if not x in stopwords_eng]
+  useful_words_string = ' '.join(useful_words)
+  tokens = nltk.word_tokenize(useful_words_string)
+  stems = stem_words(tokens, stemmer)
+  return stems
+from sklearn.feature_extraction.text import TfidfVectorizer
+tfidf = TfidfVectorizer(
+    tokenizer = tokenizer,
+    stop_words = 'english'
+)
+tfidf_matrix = tfidf.fit_transform(df['description'])
+from sklearn.metrics.pairwise import cosine_similarity
+cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)
+indices = pd.Series(df.index, index=df['id']).drop_duplicates()
+import pickle
+with open('data.pkl', 'wb') as f:
+    pickle.dump(cosine_sim, f)
+    
+with open('indices.pkl', 'wb') as f:
+    pickle.dump(indices, f)
